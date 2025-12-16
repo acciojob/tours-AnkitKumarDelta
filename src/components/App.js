8f0e97d7bfac578bbebf2b2b075c84a2f5262bab
AnkitKumarDelta <ankitna355@gmail.com>
2025-12-16 13:26:18 +0530

    commit saved

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,42 @@
 import React, { useEffect, useState } from "react";
 import Loading from "./Loading";
 import Tours from "./Tours";
+import data from '../tours.json';
 
 const App = () => {
   const[tours, setTours] = useState([]);
+  const[loading, setLoading] = useState(true);
+
   useEffect(()=>{
-    fetch('https://course-api.com/react-tours-project')
-    .then(res=>res.json())
-    .then(res=>setTours(res));
+    setTours(data);
+    setLoading(false);
   },[]);
 
+  function refreshTours(){
+    setTours(data);
+  }
+  
   function handleTours(id){
     const new_tours = tours.filter(tour=>tour.id!==id);
     setTours(new_tours);
   }
+
+  if(loading){
+    return <Loading/>
+  }
+
+  if(tours.length==0){
+    return(
+      <main>
+        <h2>No tours left</h2>
+        <button onClick={refreshTours}>Refresh</button>
+      </main>
+    );
+  }
+
     return(
       <main id="main">
-        {tours==[] && <Loading/>}
+        {tours.length==0 && <Loading/>}
         <Tours tours={tours} removeTours={handleTours}/>
       </main>
     )
--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Tour = ({tour, removeTour}) => {
+    const[toggle, setToggle] = useState(true);
   return (
     <div>
         <p>{tour.name}</p>
-        <p>{tour.info}</p>
+        <p>{toggle ? tour.info.substring(0,200)+"..." : tour.info}</p>
+        <button onClick={()=>setToggle(!toggle)}>
+            {toggle==true ? "Show more" : "Show less"}
+            </button>
         <img src={tour.image}/>
         <p>{tour.price}</p>
+        <button onClick={()=>removeTour(tour.id)}>Remove</button>
     </div>
   )
 }
