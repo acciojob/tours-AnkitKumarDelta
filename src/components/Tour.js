5c3a691a5a180146f2fb71061560d965fcf3a8c6
AnkitKumarDelta <ankitna355@gmail.com>
2025-12-16 13:33:44 +0530

    commit changes

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,29 +1,32 @@
 import React, { useState } from "react";
 
 const Tour = ({ tour, removeTour }) => {
-  const [showMore, setShowMore] = useState(false);
+    const [showMore, setShowMore] = useState(false);
 
-  return (
-    <article>
-      <img src={tour.image} alt={tour.name} width="300" />
-      <h4>{tour.name}</h4>
-      <h4>₹{tour.price}</h4>
+    return (
+        <article>
+            <img src={tour.image} alt={tour.name} width="300" />
+            <h4>{tour.name}</h4>
+            <h4>₹{tour.price}</h4>
 
-      <p id={`tour-item-para-${tour.id}`}>
-        {showMore ? tour.info : tour.info.substring(0, 200) + "..."}
-        <button onClick={() => setShowMore(!showMore)}>
-          {showMore ? "Show less" : "Show more"}
-        </button>
-      </p>
+            <p id={`tour-item-para-${tour.id}`}>
+                {showMore ? tour.info : tour.info.substring(0, 200) + "..."}
+                <button
+                    id={showMore ? `see-less-${tour.id}` : `see-more-${tour.id}`}
+                    onClick={() => setShowMore(!showMore)}
+                >
+                    {showMore ? "Show less" : "Show more"}
+                </button>
+            </p>
 
-      <button
-        id={`delete-btn-${tour.id}`}
-        onClick={() => removeTour(tour.id)}
-      >
-        Remove
-      </button>
-    </article>
-  );
+            <button
+                id={`delete-btn-${tour.id}`}
+                onClick={() => removeTour(tour.id)}
+            >
+                Remove
+            </button>
+        </article>
+    );
 };
 
 export default Tour;
